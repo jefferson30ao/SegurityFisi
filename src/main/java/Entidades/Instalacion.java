4aaaf891e4d22296a0bf3616f986b603ea5779cb
Jefferson Asencios <jefferson30.ao@gmail.com>
2024-06-23 19:16:03 -0500

    préstamo de instalaciones agregado

--- src/main/java/Entidades/Instalacion.java
+++ src/main/java/Entidades/Instalacion.java
@@ -35,20 +35,4 @@ public class Instalacion {
         }
     
 
-    public static void main(String[] args) {
-        //Instalaciones instalaciones = new Instalaciones();
-
-        // Ejemplo de cómo agregar instalaciones
-        for (int i = 1; i <= 10; i++) {
-            String nombre = JOptionPane.showInputDialog("Ingrese el nombre de la instalación " + i);
-            int codigo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el código de la instalación " + i));
-            boolean estado = JOptionPane.showConfirmDialog(null, "¿Está ocupada la instalación " + i + "?", "Estado", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
-            String prestador = JOptionPane.showInputDialog("Ingrese el nombre del prestador de la instalación " + i);
-
-            //instalaciones.agregarInstalacion(nombre, codigo, estado, prestador);
-        }
-
-        // Mostrar la tabla
-       // instalaciones.mostrarTabla();
-    }
 }
